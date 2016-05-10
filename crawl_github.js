c753eca384e959a03a6e31d79f6984654201d177
hackjustu <wenzhe89929@ucla.edu>
2016-05-09 17:41:53 -0700

    update ranking algorithms

--- crawl_github.js
+++ crawl_github.js
@@ -64,7 +64,18 @@ var crawl_github = function (production) {
 
                     /* sort the members based on events */
                     member_events.sort(function (a, b) {
-                        return b['Total'] - a['Total']
+
+                        if (b['Total'] != a['Total']) {
+                            return b['Total'] - a['Total'];
+                        } else if (b['PushEvent'] != a['PushEvent']) {
+                            return b['PushEvent'] - a['PushEvent'];
+                        } else if (b['PullRequestEvent'] != a['PullRequestEvent']) {
+                            return b['PullRequestEvent'] - a['PullRequestEvent'];
+                        } else if (b['PullRequestEvent'] != a['PullRequestEvent']) {
+                            return b['PullRequestEvent'] - a['PullRequestEvent'];
+                        } else {
+                            return b['CreateEvent'] - a['CreateEvent'];
+                        }
                     });
 
                     /* retrieve members' last ranking from Firebase */
